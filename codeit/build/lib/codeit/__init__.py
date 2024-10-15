9cbe5675b537598cbed17ba46aa86ba436027f84
Chandrahas Aroori <chandrahas.aroori@gmail.com>
2024-10-15 21:09:02 +0000

    Init file setup

--- codeit/build/lib/codeit/__init__.py
+++ codeit/build/lib/codeit/__init__.py
@@ -5,5 +5,5 @@ import os
 
 location_init_file = os.path.dirname(os.path.realpath(__file__))
 print(location_init_file)
-PROJECT_FOLDER_PATH = location_init_file.split("codeit")[0] + 'codeit'
+PROJECT_FOLDER_PATH = location_init_file.split("codeit")[0]
 print(PROJECT_FOLDER_PATH)  
